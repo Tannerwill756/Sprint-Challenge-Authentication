a1a4b035f095831a17b7bf835605502337ee574e
Tanner Williams <tannerwill756@gmail.com>
2020-05-22 13:00:39 -0400

    testing get jokes with authentication done, mvp

--- jokes/jokes-router.test.js
+++ jokes/jokes-router.test.js
@@ -5,3 +5,19 @@ const db = require("../database/dbConfig");
 beforeEach(async () => {
   await db("users").truncate();
 });
+
+test("GET /api/jokes to get all jokes, auth required", async () => {
+  const register = await request(server)
+    .post("/api/auth/register")
+    .send({ username: "justin", password: "pass" });
+  const login = await request(server)
+    .post("/api/auth/login")
+    .send({ username: "justin", password: "pass" });
+  const res = await request(server)
+    .get("/api/jokes")
+    .set("authorization", login.body.token);
+
+  expect(res.body).toHaveLength(20);
+  expect(res.body[0]).toHaveProperty("id");
+  expect(res.body[0]).toHaveProperty("joke");
+});
