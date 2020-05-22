7e085978b1c4d4b9e34e7d083ee310b23f843668
Tanner Williams <tannerwill756@gmail.com>
2020-05-22 11:37:33 -0400

    users model built and register working

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,35 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcryptjs = require("bcryptjs");
 
-router.post('/register', (req, res) => {
-  // implement registration
+const Users = require("../users/users-model.js");
+
+router.post("/register", (req, res) => {
+  const credentials = req.body;
+
+  if (isValid(credentials)) {
+    const hash = bcryptjs.hashSync(credentials.password, 8);
+    credentials.password = hash;
+
+    Users.add(credentials)
+      .then((user) => {
+        res.status(201).json({ data: user });
+      })
+      .catch((err) => {
+        res.status(500).json({ err: err.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "Please provide username and password",
+    });
+  }
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
 });
+
+function isValid(user) {
+  return Boolean(user.username && user.password);
+}
 
 module.exports = router;
